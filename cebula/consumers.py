3686575a26360b1411f663a0fbf50b906b78539f
WangHyungJun <whj2013123218@gmail.com>
2018-11-07 16:21:38 +0200

    asy

--- cebula/consumers.py
+++ cebula/consumers.py
@@ -3,30 +3,31 @@ from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 import json, pdb
 from asgiref.sync import async_to_sync
 
-class TestConsumer(WebsocketConsumer):
+class TestConsumer(AsyncWebsocketConsumer):
 
-    def connect(self):
+    async def connect(self):
         # Join room group
         self.group_name="likes"
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async  def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.group_name,
             {
                 'type': 'like_message',
@@ -35,11 +36,11 @@ class TestConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def like_message(self, event):
+    async def like_message(self, event):
         message = "%s님이 게시물을 좋아합니다."%event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
